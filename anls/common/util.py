44c7901b469a38c04587b20fcb8b48bb742e833d
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2022-06-21 22:58:24 +0900

    create function for loading jsons

--- anls/common/util.py
+++ anls/common/util.py
@@ -5,17 +5,25 @@ import pathlib
 from dataclasses import asdict, is_dataclass
 from typing import Any, Dict
 
-from anls.evaluation.schema import EvalResult
+from anls.evaluation.schema import EvalResult, GoldLabelJson, SubmissionJson
 
 logger = logging.getLogger(__name__)
 
 
-def load_json(file_path: os.PathLike) -> Dict[str, Any]:
+def _load_json(file_path: os.PathLike) -> Dict[str, Any]:
     logger.info(f"Load json from {file_path}")
 
     with open(file_path, "r") as rf:
         json_dict = json.load(rf)
     return json_dict
+
+
+def load_gold_label_json(gold_label_file_path: os.PathLike) -> GoldLabelJson:
+    return _load_json(gold_label_file_path)  # type: ignore
+
+
+def load_submission_json(submission_file_path: os.PathLike) -> SubmissionJson:
+    return _load_json(submission_file_path)  # type: ignore
 
 
 def save_json(file_path: pathlib.Path, data: EvalResult) -> None:
